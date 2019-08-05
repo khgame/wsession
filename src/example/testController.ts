c78e39591f4eae7c05d7a58b5544af746b2c91f9
bagaking <zfaustk@qq.com>
2019-08-06 03:12:31 +0800

    package: rename package to wsession

--- src/example/testController.ts
+++ src/example/testController.ts
@@ -24,7 +24,7 @@ export class TestController {
      * 带参数调用的示例, 监听事件 3
      * 有参数时, 用 WSParam 对应 data 内字段名,
      * 用 Ctx 对应 Context 对象
-     * 未指定会报消息号, 返回值不会发送给客户端
+     * 未指定回报消息号, 返回值不会发送给客户端
      */
     @WSHandler(3)
     async method3(@WSParam("arg1") input: number, @WSCtx() ctx: WSContext) {
@@ -37,7 +37,7 @@ export class TestController {
     /**
      * 带参数, 不用 WSParam 的示例
      * 参数固定为 data 和 context
-     * 有会报消息号时, 返回值会被发送给客户端
+     * 有回报消息号时, 返回值会被发送给客户端
      */
     @WSHandler(4, 5)
     async method4(data: { m: string }, ctx: WSContext) {
