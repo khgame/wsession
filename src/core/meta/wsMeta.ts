6c27a1cf156b4e171dccaf1c4e4116c5982b726f
bagaking <zfaustk@qq.com>
2019-08-12 16:37:36 +0800

    feat (meta): add inject method to meta

--- src/core/meta/wsMeta.ts
+++ src/core/meta/wsMeta.ts
@@ -1,5 +1,7 @@
 export class WSMeta {
 
+    static injectTable: Map<Function, any> = new Map<Function, any>();
+
     static targetTable: Map<Function, WSMeta> = new Map<Function, WSMeta>();
 
     static create(
@@ -8,6 +10,16 @@ export class WSMeta {
     ) {
         const targetMeta = new WSMeta(targetClass, option);
         this.targetTable.set(targetClass, targetMeta);
+    }
+
+    static inject<T>(targetClass: Function, instance: any) {
+        return this.injectTable.set(targetClass, instance);
+    }
+
+    static uninject<T>(targetClass: Function, instance: any) {
+        if (this.injectTable.get(targetClass) === instance) {
+            this.injectTable.delete(targetClass);
+        }
     }
 
     static find(targetClass: Function): WSMeta {
@@ -28,10 +40,10 @@ export class WSMeta {
 
     get instance() {
         if (!this._instance) {
-            this._instance = new (this.targetClass as any)();
+            this._instance = WSMeta.injectTable.get(this.targetClass) || new (this.targetClass as any)();
         }
         return this._instance;
-    };
+    }
 
 }
 
